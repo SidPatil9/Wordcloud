aa0acbd86bd65156575e880ed1569c189e1bb96e
siddharth.patil <siddharth.patil.smp@gmail.com>
2024-04-04 17:37:21 +0530

    ex_list updated

--- automate.py
+++ automate.py
@@ -25,7 +25,7 @@ for i in desc:
 uniques = list(set(words))
 
 # create a list of words to exclude from the analysis
-ex_list = ['overall', 'this', 'will', 'sure', 'just', 'both', 'gender', 'also', 'while', 'your', "you're", 'google', 'that', 'more', 'kale', 'have', 'with', 'at', 'on', 'the', 'within', 'and', 'you', 'atleast', 'like', 'mentioned', 'below', 'current', 'location', 'role']
+ex_list = ['your', 'yourself', 'from', 'part', 'such', 'overall', 'this', 'will', 'sure', 'just', 'both', 'gender', 'also', 'while', 'your', "you're", 'google', 'that', 'more', 'kale', 'have', 'with', 'at', 'on', 'the', 'within', 'and', 'you', 'atleast', 'like', 'mentioned', 'below', 'current', 'location', 'role']
 
 # create a blank list to populate later with only significant words
 words_uniques = []
@@ -47,9 +47,8 @@ word_cloud_dict = dict(zip(keys, values))
 word_cloud_dict = {k: v for k, v in sorted(word_cloud_dict.items(), key=lambda item: item[1], reverse = True)}
 
 # choose the top N keys to include in the histogram
-#word_cloud = dict(list(word_cloud_dict.items())[0:50])
+word_cloud = dict(list(word_cloud_dict.items())[0:10])
 word_cloud = dict( (key, value) for (key, value) in word_cloud_dict.items() if value > 1)
-print(word_cloud)
 
 # create the histogram
 import matplotlib.pyplot as plt
